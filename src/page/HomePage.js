4022b861db904e075071aae1e951f0fcc73d59ad
CUMT-Atom <ccatom@88.com>
2024-10-10 20:00:54 +0800

    [bugfix]HOST未调用

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,8 +1,10 @@
+import {HOST} from "../config";
+
 function HomePage(){
     return (
         // eslint-disable-next-line jsx-a11y/iframe-has-title
         <div style={{width: '100%', height: '100%'}} >
-            <iframe style={{width: '100%', height: '100%',border:'0px'}} src="http://localhost:3000/"></iframe>
+            <iframe style={{width: '100%', height: '100%',border:'0px'}} src={`http://${HOST}:3000/`}></iframe>
         </div>
     )
 }
